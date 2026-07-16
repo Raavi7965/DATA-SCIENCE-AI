aa11f1527c25f35afaa6b8fcac818f028cde193e
Raavi7965 <venkiravi796@gmail.com>
2026-07-16 10:57:06 +0530

    first commit

---
+++ Datatypes D3.py
@@ -0,0 +1,23 @@
+# Datatypes in Python
+str='10'
+print(type(str))
+
+#Numerical Datatypes
+# integer: It is a whole number with no decimal point, it can be positive or negative.
+#type function type() is used to check the data type
+
+int1=10
+age=25
+Class=11
+temp=-31
+print(type(age))
+print(type(Class))
+print(type(temp))
+print(type(int1))
+
+#Float: It is a number with a decimal point, it can be positive or negative or Zero
+percentage =9.99
+cgpa=7.79
+height = 167.5
+print(type(percentage))
+
