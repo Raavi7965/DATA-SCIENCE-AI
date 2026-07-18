6e40516015e26bdb88a2e42fcdb0d0f44d9e20f5
Raavi7965 <venkiravi796@gmail.com>
2026-07-18 11:24:17 +0530

    Convert Python folder to normal files (remove submodule reference)

---
+++ Python/Datatypes D3.py
@@ -0,0 +1,75 @@
+# Datatypes in Python
+str='10'
+print(type(str))
+
+#Numerical Datatypes
+# integer: It is a whole number with no decimal point, it can be positive or negative.
+#type function type() is used to check the data type
+
+int1=10
+age=25
+Class=11
+temp=-31
+print(type(age))
+print(type(Class))
+print(type(temp))
+print(type(int1))
+
+#Float: It is a number with a decimal point, it can be positive or negative or Zero
+percentage =9.99
+cgpa=7.79
+height = 167.5
+print(type(percentage))
+
+# Complex: It is a combination of real number and integer number
+
+Com1 = 2+5j
+com2 = 2-5j
+com3 = 5.4+4j
+print(type(Com1))
+
+#String Data Types
+#It is a group of characters given between the quotes.
+
+name="venkatesh" 
+username='venkatesh7965'
+Password="venkatesh@123"
+empty = " "
+print(type(name))
+print(type(username))
+print(type(Password))
+print(type(empty))
+
+#Boolean Data Types
+#It is a data type which can have only two values True or False. 
+# It is used to represent the truth value of an expression.
+print(5==3)
+print(5>3)
+
+
+#sequence Data Types
+#Object 
+#List and Tuple are the data types which can store multiple values in a single variable.
+#List: It is a collection of values which can be of different data types. It is
+
+#List
+Sem1Marks=[90,80,70,60,50]
+print(type(Sem1Marks))
+print(Sem1Marks)
+
+Profile = ['Venkatesh', 'B.Tech',25,None,820,['Python','Java','C']]
+print(type(Profile))
+print(Profile)
+
+#Tuple: It is a collection of values which can be of different data types. 
+# It is similar to list but it is immutable i.e. we cannot change the values once it is created.
+
+tuple1=(1,2,3,4,5)
+print(type(tuple1))
+
+
+
+
+
+
+
